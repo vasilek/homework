230a8850919c1bdd9851f9cb3a29adaee25f62e2
vasilek <vasiltivkov@gmail.com>
2017-03-08 16:25:24 +0300

    добавил redirect

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, combineReducers } from 'redux'
-import { Router, Route, hashHistory } from 'react-router'
+import { Router, Route, hashHistory, Redirect } from 'react-router'
 import personlist from './reducers/personlist'
 import { reducer as formReducer } from 'redux-form'
 import Form from './Form'
@@ -12,18 +12,20 @@ import Show from './Show'
 const reducers = {
   personlist,
   form: formReducer
-}
-const reducer = combineReducers(reducers)
-const store = createStore(reducer)
+};
+
+const reducer = combineReducers(reducers);
+const store = createStore(reducer);
 
 render(
   <Provider store={store}>
   <Router history={hashHistory}>
 
             <Route path="/users/create" component={Form}/>
+            <Redirect from="/" to="/users/create" />
             <Route path="/users/list" component={Show}/>
               </Router>
   </Provider>,
   document.getElementById('root')
-)
+);
 
